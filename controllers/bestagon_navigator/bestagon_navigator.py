0b2bd06443b2b8d684862f433e7374ab848dc37f
Rami Sabbagh <rami.sab07@gmail.com>
2023-12-28 20:11:30 +0300

    Color and LiDAR sensors for boxes

--- controllers/bestagon_navigator/bestagon_navigator.py
+++ controllers/bestagon_navigator/bestagon_navigator.py
@@ -1,7 +1,7 @@
 """bestagon_navigator controller."""
 
 import numpy as np
-from controller import Robot
+from controller import Robot, Lidar, Camera
 
 from typing import *
 from numpy.typing import NDArray
@@ -13,11 +13,19 @@ robot = Robot()
 TIMESTEP = int(robot.getBasicTimeStep())
 CONTROL_TIMESTEP = TIMESTEP * 4
 
-for name, device in robot.devices.items():
-    print(name, type(device))
+# for name, device in robot.devices.items():
+#     print(name, type(device))
 
 # -- Devices -- #
 wheels_driver = MecanumDriver(robot)
+lidar: Lidar = robot.getDevice('front-box-lidar')
+camera: Camera = robot.getDevice('front-box-cam')
+
+lidar.enable(100)
+camera.enable(100)
+#lidar.disable()
+
+print('RES:', lidar.getHorizontalResolution())
 
 
 def sleep(time: float) -> None:
@@ -25,6 +33,13 @@ def sleep(time: float) -> None:
 
 
 while robot.step(CONTROL_TIMESTEP) != -1:
-    pass
+    sleep(2)
+    # data = lidar.getRangeImage()
+    data = np.frombuffer(camera.getImage(), dtype=np.uint8)
+    if data is None: continue
+    # data = data[::4]
+
+    data = list(map(lambda x: '-' if x > 1e10 else f'{x}', data))
+    print(' '.join(data))
 
 # Enter here exit cleanup code.
