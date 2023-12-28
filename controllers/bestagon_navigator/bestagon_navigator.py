90c298cda19a8ecbcba40e1db2b3e9967336410b
Rami Sabbagh <rami.sab07@gmail.com>
2023-12-28 17:16:07 +0300

    Back into the hexagon

--- controllers/bestagon_navigator/bestagon_navigator.py
+++ controllers/bestagon_navigator/bestagon_navigator.py
@@ -13,32 +13,18 @@ robot = Robot()
 TIMESTEP = int(robot.getBasicTimeStep())
 CONTROL_TIMESTEP = TIMESTEP * 4
 
-# for name, device in robot.devices.items():
-#     print(name, type(device))
+for name, device in robot.devices.items():
+    print(name, type(device))
+
+# -- Devices -- #
+wheels_driver = MecanumDriver(robot)
 
 
 def sleep(time: float) -> None:
     robot.step(int(time * 1000))
 
 
-wheels_driver = MecanumDriver(robot)
-
-# motors_driver.normalized_motors_velocity = [1, 0, 0, 0]
-# sleep(1)
-# motors_driver.normalized_motors_velocity = [0, 1, 0, 0]
-# sleep(1)
-# motors_driver.stop()
-
-wheels_driver.move('left')
-sleep(.3)
-
-angle = .5 * np.pi
-
 while robot.step(CONTROL_TIMESTEP) != -1:
-    dt = CONTROL_TIMESTEP / 1000
-    angle += dt * np.pi * .5
-    angle %= np.pi * 2
-
-    wheels_driver.move(angle, turn=np.pi*.1)
+    pass
 
 # Enter here exit cleanup code.
