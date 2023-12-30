288039393244b5fb58c9c102a048fdbd6b4713fd
Rami Sabbagh <rami.sab07@gmail.com>
2023-12-30 14:54:01 +0300

    Introducing `YoubotArmDriver`

--- controllers/bestagon_navigator/bestagon_navigator.py
+++ controllers/bestagon_navigator/bestagon_navigator.py
@@ -7,6 +7,7 @@ from typing import *
 from numpy.typing import NDArray
 
 from mecanum_driver import MecanumDriver
+from youbot_arm_driver import YoubotArmDriver
 
 robot = Robot()
 
@@ -18,28 +19,32 @@ CONTROL_TIMESTEP = TIMESTEP * 4
 
 # -- Devices -- #
 wheels_driver = MecanumDriver(robot)
-lidar: Lidar = robot.getDevice('front-box-lidar')
-camera: Camera = robot.getDevice('front-box-cam')
+# lidar: Lidar = robot.getDevice('front-box-lidar')
+# camera: Camera = robot.getDevice('front-box-cam')
 
-lidar.enable(100)
-camera.enable(100)
+# lidar.enable(100)
+# camera.enable(100)
 #lidar.disable()
 
-print('RES:', lidar.getHorizontalResolution())
+# print('RES:', lidar.getHorizontalResolution())
 
 
 def sleep(time: float) -> None:
     robot.step(int(time * 1000))
 
 
+arm_driver = YoubotArmDriver(robot, CONTROL_TIMESTEP)
+arm_driver.pose('reset')
+
 while robot.step(CONTROL_TIMESTEP) != -1:
-    sleep(2)
+    # sleep(2)
     # data = lidar.getRangeImage()
-    data = np.frombuffer(camera.getImage(), dtype=np.uint8)
-    if data is None: continue
+    # data = np.frombuffer(camera.getImage(), dtype=np.uint8)
+    # if data is None: continue
     # data = data[::4]
 
-    data = list(map(lambda x: '-' if x > 1e10 else f'{x}', data))
-    print(' '.join(data))
+    # data = list(map(lambda x: '-' if x > 1e10 else f'{x}', data))
+    # print(' '.join(data))
+    pass
 
 # Enter here exit cleanup code.
